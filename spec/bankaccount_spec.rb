39374c9826d2c6092424764e35b4336a46538184
Aaron <aaronrod@gmail.com>
2017-07-10 17:26:58 +0100

    Adds Tests for withdrawing and depositing money into an account -All Tests Pass

--- spec/bankaccount_spec.rb
+++ spec/bankaccount_spec.rb
@@ -7,4 +7,22 @@ describe BankAccount do
   it 'should exist' do
     expect(bankaccount).to be_truthy
   end
+
+  it 'should have a balance of zero when opened' do
+    expect(bankaccount.balance).to eq(0)
+  end
+
+  context 'Deposit' do
+    it 'should add money to an account' do 
+      bankaccount.deposit(1)
+      expect(bankaccount.balance).to eq 1
+    end
+  end
+  context 'Withdraw' do
+    it 'should subtract money from an account' do
+      bankaccount.deposit(1)
+      bankaccount.withdraw(1)
+      expect(bankaccount.balance).to eq 0
+    end
+  end
 end
